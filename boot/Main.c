336aadcee9ec484cb94d41eb49f191d6d00b75d5
owlHJ <rlaguswns901@gmail.com>
2024-09-11 19:56:37 -0700

    OS develop Part5 & first git

---
+++ boot/Main.c
@@ -0,0 +1,56 @@
+#include "stdint.h"
+#include "HalUart.h"
+
+#include "stdio.h"
+
+static void Hw_Init(void);
+static void Printf_test(void);
+
+void main(void)
+{
+
+    Hw_Init();
+
+    uint32_t i = 100;
+    while ((--i))
+    {
+        /* code */
+        //Hal_uart_put_char("1");
+        Hal_uart_put_char('a');
+    }
+    
+    Hal_uart_put_char('\n');
+
+    putstr("Hello World!\n");
+
+    Printf_test();
+
+    i = 100;
+    
+    while(i--)
+    {
+        uint8_t ch = Hal_uart_get_char();
+        Hal_uart_put_char(ch);
+    }
+    // uint32_t* dummyAddr = (uint32_t*)(1024*1024*100);
+    // *dummyAddr = sizeof(long);
+}
+static void Hw_Init(void)
+{
+    Hal_uart_init();
+}
+static void Printf_test(void)
+{
+    char* str       = "printf pointer test";
+    char* nullptr   = 0;
+    uint32_t i      = 5;
+
+    debug_printf("%s\n", "Hello printf");
+    debug_printf("output string pointer: %s\n", str );
+    debug_printf("%s is null pointer, %u number\n", nullptr, 10);
+    debug_printf("%u = 5\n", i);
+    debug_printf("dec=%u hex=%x\n",0xff,0xff);
+    debug_printf("print zero %u\n",0);
+
+    return;
+}
